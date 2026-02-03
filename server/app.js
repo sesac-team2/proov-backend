3152b6392f68f2b72792fe92e9d2d9d8934c8dc2
wjddns0122 <jwoon2004@gmail.com>
2026-02-03 22:53:34 +0900

    [feat] CI/CD 테스트

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Test");
+    res.send("CI/CD Test 배포 자동화 테스트");
 });
 
 app.listen(5000, () => {
