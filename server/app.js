ba9641fec92ad31f5709c5ad256874fca56c02ea
wjddns0122 <jwoon2004@gmail.com>
2026-02-12 21:28:12 +0900

    [hotfix] - routes 해결

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import projectRoute from "./routes/projectRoute.js";
+import testimonialRoute from "./routes/testimonialRoute.js";
 import publicShareRoute from "./routes/publicShareRoute.js";
 import { swaggerUi, specs } from "./config/swagger.js";
 import cors from "cors";
